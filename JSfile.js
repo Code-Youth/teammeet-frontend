f8504165c1381ec9a031d6b502f9f0f68747c333
Majid Mukhtar <majidmukhtar837@gmail.com>
2021-10-28 09:37:54 -0400

    Majids Updates

--- JSfile.js
+++ JSfile.js
@@ -1,14 +1,32 @@
-function getGames() {
+/*
+function getMovies() {
 
-    let searchInput = document.getElementById("search_input")
+    
 
-    fetch("https://www.omdbapi.com/?apikey=b6791dd0&s=" + searchInput.value)
-    .then((resp) => resp.json())
-    .then(function(data) {
-        console.log(data.Search)
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
+    var request = new XMLHttpRequest();
+
+   
+    request.open('GET', 'http://www.omdbapi.com/?t=joker&y=&plot=short&r=json&apikey=b6791dd0');
+    request.send();
+    request.onload = ()=>{
+        let movie_input = window.prompt();
+        let parsedResponse = JSON.parse(request.response)
+        console.log(parsedResponse);
+    }
 }
+*/
 
+function changeHtmlContentsById(){
+
+    var request = new XMLHttpRequest();
+
+    request.open('GET', 'http://www.omdbapi.com/?t=joker&y=&plot=short&r=json&apikey=b6791dd0');
+    request.send();
+    
+    request.onload = ()=>{
+        let parsedResponse = JSON.parse(request.response)
+        p1_element = document.getElementById("p1")
+
+        p1_element.innerHTML = "movie is " + parsedResponse.Title
+    }
+  }
