e6ab0a413f9f8a149638e21c4661df32090cb8a8
carter-kilgour <71785488+carter-kilgour@users.noreply.github.com>
2021-11-12 08:53:48 -0500

    merge conflicts

--- JSfile.js
+++ JSfile.js
@@ -24,7 +24,7 @@ function appendGameCard(game) {
     
     let img = document.createElement("img")
     img.setAttribute("id","soccer_pic")
-    img.setAttribute("src",movie.Poster)
+    //img.setAttribute("src",movie.name)
     img.setAttribute("class",'img-fluid')
     
     let colDiv = document.createElement("div")
@@ -32,16 +32,16 @@ function appendGameCard(game) {
 
     let h3 = document.createElement("h3")
     h3.setAttribute("class", "soccer_title")
-    h3.innerHTML = movie.Title
+    h3.innerHTML = game.name
 
     let h4 = document.createElement("h4")
-    h4.innerHTML = movie.Type
+    h4.innerHTML = game.sport + game.location + game.date
 
     h3.appendChild(h4)
     colDiv.appendChild(h3)
     imgDiv.appendChild(img)
     rowDiv.appendChild(colDiv)
-    rowDiv.appendChild(imgDiv)
+    //rowDiv.appendChild(imgDiv)
     containerFluidDiv.appendChild(rowDiv)
     jumbotronDiv.appendChild(containerFluidDiv)
 
@@ -54,17 +54,14 @@ function appendGameCard(game) {
 
 function getGames() {
 
-    let searchInput = document.getElementById("search_input")
-
-    fetch("https://www.omdbapi.com/?apikey=b6791dd0&s=" + searchInput.value)
+    fetch("https://teammeet-backend-app.azurewebsites.net/api/games")
     .then((resp) => resp.json())
     .then(function(data) {
-        let movies = data.Search
 
-        console.log(movies)
+        let games = data.games
 
-        for (let i = 0; i < movies.length; i++) {
-            appendGameCard(movies[i])
+        for (let i = 0; i < games.length; i++) {
+            appendGameCard(games[i])
         } 
     })
     .catch(function(error) {
