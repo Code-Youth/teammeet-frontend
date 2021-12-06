8be9d11e9fa0debf8f4895fe7159b2d0d4b92c20
carter-kilgour <71785488+carter-kilgour@users.noreply.github.com>
2021-12-05 22:48:54 -0500

    cleanup + working version

--- js/cookies.js
+++ js/cookies.js
@@ -4,7 +4,7 @@ function setCookie(cname, cvalue, exdays, settings="") {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;" + settings;
+    document.cookie = cname + "=" + cvalue + ";path=/;" + settings;
 }
 
 function getCookie(cname) {
--- public/README.md
+++
@@ -1,2 +0,0 @@
-# Teem-meet-Front-End
-The visual portion of our website
